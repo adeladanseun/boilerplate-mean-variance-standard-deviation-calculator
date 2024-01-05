c778036700fc1597cbc2daecfcd2aa5d76b51640
adeladanseun <76960202+adeladanseun@users.noreply.github.com>
2024-01-05 01:39:25 +0100

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,13 +1,55 @@
 import numpy as np
 
 def calculate(list):
-    a = np.array(list).reshape((3,3))
-    calculations = {}
-    for items in a:
-        items = np.array(items)
-        
-
-
-
+    a = np.array(list)
+    if len(a)!=9:
+        raise ValueError
+    else: 
+        flatMin = a.min()
+        flatMax = a.max()
+        flatVar = a.var()
+        flatStd = a.std()
+        flatSum = a.sum()
+    a=a.reshape((3,3))
+    calculations = {'min':[], 'max':[], 'var':[], 'std':[], 'sum':[]}
+    for i in range(2):
+        newMin=[]
+        newMax=[]
+        var=[]
+        std=[]
+        sum=[]
+        for j in range(a):
+            if i==1:
+                row = a[j]
+            else:
+                row = a[: ,j]
+            newMin.append(row.min())
+            newMax.append(row.max())
+            var.append(row.var())
+            std.append(row.std())
+            sum.append(row.sum())
+        calculations['min'].append(newMin)
+        calculations['max'].append(newMax)
+        calculations['var'].append(var)
+        calculations['std'].append(std)
+        calculations['sum'].append(sum)
+    calculations['sum'].append(flatSum)
+    calculations['std'].append(flatStd)
+    calculations['var'].append(flatVar)
+    calculations['min'].append(flatMin)
+    calculations['max'].append(flatMax)
 
     return calculations
+
+
+
+
+
+
+
+
+
+
+
+
+
